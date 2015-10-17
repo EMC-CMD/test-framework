d7892e719f8503a18099b68ded8fbfa98da13125
vagrant <vagrant@mesos.vm>
2015-10-17 20:32:34 +0000

    gopath set to /vagrant/

--- boostrap.sh
+++ boostrap.sh
@@ -2,7 +2,7 @@
 # This script is meant for quick & easy install via:
 #   'curl -sSL https://raw.githubusercontent.com/EMC-CMD/test-framework/master/boostrap.sh | sh'
 set -e
-wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go1.5.1.linux-amd64.tar.gz && chmod +x go1.5.1.linux-amd64.tar.gz && export GOPATH=/vagrant/my_go && export PATH=$PATH:/usr/local/go/bin && rm go1.5.1.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go1.5.1.linux-amd64.tar.gz && chmod +x go1.5.1.linux-amd64.tar.gz && export GOPATH=/vagrant/ && export PATH=$PATH:/usr/local/go/bin && rm go1.5.1.linux-amd64.tar.gz
 sudo apt-get update && sudo apt-get install software-properties-common git -y && sudo apt-get update && sudo add-apt-repository ppa:ubuntu-lxc/stable && sudo apt-get update && sudo apt-get install -y libprotobuf-c0 && sudo apt-get install -y criu
 curl -sSL -O https://github.com/boucher/docker/releases/download/v1.9.0-experimental-cr.1/docker-1.9.0-dev && chmod +x docker-1.9.0-dev && sudo mv docker-1.9.0-dev /usr/local/bin/docker
 sudo docker daemon &
